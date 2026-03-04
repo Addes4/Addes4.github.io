83f820caad3d298a0d709a30a5c410a6e8dadc9a
Addes4 <asohrabi@kth.se>
2026-03-04 14:52:00 +0100

    revser order events

--- data/involvement.ts
+++ data/involvement.ts
@@ -15,22 +15,22 @@ export interface EventEntry {
 
 export const events: EventEntry[] = [
   {
-    date: 'April 2025',
-    name: 'KTH AIS x Microsoft x SSE Business Lab Hackathon',
+    date: 'February 2026',
+    name: 'Hackeurope 2026',
     organization: 'KTH AI Society',
-    description: 'A 2 week long hackathon with 50+ participants. We partnered with Microsoft and SSE Business Lab to host the hackathon.',
+    description: 'Was a judge and organizer for a 24-hour hackathon with 300+ participants. We partnered with HackEurope, Susquehanna International Group and Founders House to host the hackathon.',
   },
   {
-    date: 'November 2025',
-    name: 'KTH AIS x Open AI x Stripe Hackathon',
+    date: 'February 2026',
+    name: 'KTH AIS x Jane Street Partnership',
     organization: 'KTH AI Society',
-    description: 'A 24-hour hackathon with 60+ participants. We partnered with Open AI, Stripe and Founders House to host the hackathon.',
+    description: 'I initiated a partnership with Jane Street to get our students access to the best resources and opportunities in the industry. We are now hosting a series of events with Jane Street.',
   },
   {
-    date: 'December 2025',
-    name: 'KTH AIS x Mannheimer Swartling Workshop',
+    date: 'February 2026',
+    name: 'KTH AIS x Lovable x Florent Venture Partners x Google x Microsoft x Listen Labs x Qualia x Specific AI Hackathon',
     organization: 'KTH AI Society',
-    description: 'A workshop with 60+ participants. We partnered with Mannheimer Swartling to hear more about AI legislation and regulations.',
+    description: 'A 24-hour hackathon with 50+ participants. The goal was to build a multi-agent system that could help with a variety of tasks.',
   },
   {
     date: 'January 2026',
@@ -39,16 +39,28 @@ export const events: EventEntry[] = [
     description: 'A panel discussion and coffee chats with Y-Combinator and Vesence. Our students had the opportunity to pitch their ideas to Y-Combinator and get feedback from 2 YC partners.',
   },
   {
-    date: 'February 2026',
-    name: 'KTH AIS x Jane Street Partnership',
+    date: 'December 2025',
+    name: 'KTH AIS x Mannheimer Swartling Workshop',
     organization: 'KTH AI Society',
-    description: 'Initiated a partnership with Jane Street to get our students access to the best resources and opportunities in the industry. We are now hosting a series of events with Jane Street.',
+    description: 'A workshop with 60+ participants. We partnered with Mannheimer Swartling, the nordics biggest law firm,  to hear more about AI legislation and regulations.',
   },
   {
-    date: 'February 2026',
-    name: 'Hackeurope 2026',
+    date: 'November 2025',
+    name: 'KTH AIS x Open AI x Stripe Hackathon',
     organization: 'KTH AI Society',
-    description: 'A 24-hour hackathon with 300+ participants. We partnered with HackEurope, Susquehanna International Group and Founders House to host the hackathon.',
+    description: 'A 24-hour hackathon with 60+ participants. We partnered with Open AI, Stripe and Founders House to host the hackathon.',
+  },
+  {
+    date: 'October 2025',
+    name: 'KTH AIS x ',
+    organization: 'KTH AI Society',
+    description: 'A workshop with 60+ participants. We partnered with Jane Street, the largest investment bank in the world, to hear more about AI in finance.',
+  },
+  {
+    date: 'April 2025',
+    name: 'KTH AIS x Microsoft x SSE Business Lab Hackathon',
+    organization: 'KTH AI Society',
+    description: 'A 2 week long hackathon with 50+ participants. We partnered with Microsoft and SSE Business Lab to host the hackathon.',
   },
 ]
 
